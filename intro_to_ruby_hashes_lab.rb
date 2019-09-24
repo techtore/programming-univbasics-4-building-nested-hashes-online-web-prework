bf7bd0fb2bc1b2398af591d05ec8ad67d1ede5e9
Victoria Hale <vic.hale74@gmail.com>
2019-09-24 02:10:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,10 @@ def monopoly_with_fourth_tier
   base_hash = {
     :railroads =>{
     :names =>{
-      :reading_railroad => "$100",
+       :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
       pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}
       
     }, 
