7d691ad9fcce5123c71bb63a487c2fb7421b88b2
Victoria Hale <vic.hale74@gmail.com>
2019-09-24 02:00:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
     :railroads =>{
     :names =>{key1: "", key2: "", key3: "", key4: ""}, 
     :pieces => 4,  
-    :rent_in_dollars =>{:one_piece_owned: "fee of 25",
+    :rent_in_dollars =>{
       level1: "", level2: "", level3: "", level4: ""}
       
     }
