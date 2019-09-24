f0a14198deffd7ac34db5bb785d503a1525f2e4f
Victoria Hale <vic.hale74@gmail.com>
2019-09-24 01:54:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 def monopoly_with_third_tier
   base_hash = {
     :railroads =>{
-    :names =>{}, 
+    :names =>{key1: "", key2: "", key3: "", key4: ""}, 
     :pieces => 4,  
     :rent_in_dollars =>{
       level1: "", level2: "", level3: "", level4: ""}
